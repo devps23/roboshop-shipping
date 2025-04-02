f2ca2b389fd7a1a28ebecab132eb3e830881047f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-04-02 17:19:11 +0530

    backend schema

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-source /data/secrets
+zsource /data/secrets
 cat /data/secrets
 
 if [ -z "$CART_ENDPOINT" ]; then
@@ -11,9 +11,17 @@ if [ -z "$DB_HOST" ]; then
   exit 1
 fi
 
+if [ -z "$LICENSE_KEY" ]; then
+  echo "LICENSE_KEY is missing"
+  exit 1
+fi
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+#mysql -h $DB_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql  this is a schema
 # here MYSQL-SERVER-IPADDRESS is a DB_HOST
 # in our code DB_HOST is a rds end point
 
-java -jar /app/shipping.jar
+java - javaagent:newrelic/newrelic.jar -Dnewrelic.config.license_key=${LICENSE_KEY} -jar /app/shipping.jar
+
+#ENTRYPOINT ["java","-javaagent:/usr/local/newrelic/newrelic.jar","-jar","/app/my-application.jar"]
+# -Dnewrelic.config.license_key=YOUR_NEW_RELIC_LICENSE_KEY ---to add License key
+# need to add -Dnewrelic.config.app_name=shipping
